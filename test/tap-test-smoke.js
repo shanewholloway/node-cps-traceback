f4a11b2ff74d49d7d6b5c6e445222a8a2bfb1061
Shane Holloway <shane.holloway@ieee.org>
2017-07-27 19:49:58 -0600

    Added support for Traceback with a leading error.

--- test/tap-test-smoke.js
+++ test/tap-test-smoke.js
@@ -15,3 +15,14 @@ module.exports = exports = function (tap, cps_traceback, test_utils) ::
       const frames = cps_traceback.capture().toFrames()
       t.deepEqual @ test_utils.asFrameKeys(frames)
         , @: head:[], tail: @[] 'CPS[3]::PROMISE', 'CPS[2]::PROMISE', 'CPS[1]::PROMISE'
+
+  tap.test @ 'Smoke async with error', async t => ::
+    try ::
+      await t
+
+      throw new Error @ 'a test error'
+
+    catch err ::
+      const frames = cps_traceback.error(err).toFrames()
+      t.deepEqual @ test_utils.asFrameKeys(frames)
+        , @: head:[], tail: @[] "Error: a test error", "CPS[3]::PROMISE", "CPS[2]::PROMISE", "CPS[1]::PROMISE"
